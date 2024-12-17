85856100ab906ec261b24b17b2c37b66848b8eb1
nilesh1114 <nileshkr1114@gmail.com>
2024-12-18 00:21:42 +0530

    Update backend.tf

--- modules/subnet/backend.tf
+++ modules/subnet/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "powertool1107"
-    key            = "security_group/terraform.tfstate"  # State file specific to the security_group module
+    key            = "subnet/terraform.tfstate"  # State file specific to the security_group module
     region         = "us-east-1"
     encrypt        = true
     versioning     = true
