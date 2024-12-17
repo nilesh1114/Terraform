37159175bae76b5df05fc08d5c8a26c0619f6ec9
nilesh1114 <nileshkr1114@gmail.com>
2024-12-17 20:14:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 # Configure the backend for Terraform state storage using S3 and DynamoDB for state locking
 terraform {
   backend "s3" {
-    bucket         = aws_s3_bucket.terraform_state.bucket  # S3 bucket for state storage
+    bucket         = powertool1107  # S3 bucket for state storage
     key            = "file/terraform.tfstate"                  # Path for the state file
     region         = "us-east-1"                           # AWS region
-    dynamodb_table = aws_dynamodb_table.terraform_lock.name # DynamoDB table for state locking
+    dynamodb_table = powertool # DynamoDB table for state locking
     encrypt        = true                                  # Enable encryption for state files
   }
 }
