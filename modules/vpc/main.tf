12f7defe33e9e23fdf29eb955a9faec7edc35d0c
nilesh1114 <nileshkr1114@gmail.com>
2024-12-18 11:27:49 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,8 +1,31 @@
-resource "aws_vpc" "lab2" {
-  cidr_block = var.cidr_block
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "mbrapp" {
+  cidr_block = var.vpc_cidr
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = {
-    Name = var.vpc_name
-  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.mbrapp.id
+  cidr_block = element(var.public_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.mbrapp.id
+}
+
+resource "aws_route" "internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mbrapp.id
+}
+
+resource "aws_internet_gateway" "mbrapp" {
+  vpc_id = aws_vpc.mbrapp.id
 }
