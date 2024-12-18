5a9a7e262b22afaeed03c7406ce456882566ca0f
nilesh1114 <nileshkr1114@gmail.com>
2024-12-18 11:30:29 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,19 @@
-variable "vpc_name" {
-  description = "The name of the VPC."
+variable "region" {
+  description = "AWS region"
   type        = string
 }
 
-variable "cidr_block" {
-  description = "CIDR block for the VPC."
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   type        = string
+}
+
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
 }
