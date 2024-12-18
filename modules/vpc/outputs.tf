040ddf2a049b182b000a01b6f7499902d2ba3a71
nilesh1114 <nileshkr1114@gmail.com>
2024-12-18 11:31:26 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,9 @@
 output "vpc_id" {
-  value = aws_vpc.lab2.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.mbrapp.id
+}
+
+output "subnet_ids" {
+  description = "IDs of the created subnets"
+  value       = aws_subnet.public[*].id
 }
