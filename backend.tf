ed603a3c1e093110801755f77e3e649128674be0
nilesh1114 <nileshkr1114@gmail.com>
2024-12-23 03:48:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "powertool1107"
+    key            = "lab4/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     versioning     = true
