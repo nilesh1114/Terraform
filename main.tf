c61e0dad07460962bfbaf94fec2dc518cf7c1881
nilesh1114 <nileshkr1114@gmail.com>
2024-12-23 14:23:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,4 +40,16 @@ resource "aws_security_group" "web_sg" {
 
 # Provision an EC2 instance
 resource "aws_instance" "web_server" {
-  ami           
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.web_key.key_name
+  security_groups = [aws_security_group.web_sg.name]
+  
+  tags = {
+    Name = "WebServer"
+  }
+
+  # Use remote-exec to configure EC2 with Ansible
+  provisioner "remote-exec" {
+    inline = [
+      "ec
