291dca538183aaea78a07dc5978405ff3ae5ca5d
nilesh1114 <nileshkr1114@gmail.com>
2024-12-23 12:06:45 +0530

    Delete modules directory

--- modules/subnet/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "lab2" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = var.cidr_block
-  availability_zone       = var.availability_zone
-  map_public_ip_on_launch = var.map_public_ip_on_launch
-  tags = {
-    Name = var.subnet_name
-  }
-}
