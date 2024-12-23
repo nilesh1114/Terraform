ea0c1dd95056b157bf741f682a8b5febabe2f52c
nilesh1114 <nileshkr1114@gmail.com>
2024-12-23 14:24:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,29 +27,4 @@ resource "aws_security_group" "web_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Provision an EC2 instance
-resource "aws_instance" "web_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.web_key.key_name
-  security_groups = [aws_security_group.web_sg.name]
-  
-  tags = {
-    Name = "WebServer"
-  }
-
-  # Use remote-exec to configure EC2 with Ansible
-  provisioner "remote-exec" {
-    inline = [
-      "ec
+ 
