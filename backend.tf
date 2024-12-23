f4b1cbf9bea3b26277d75c1784c25509fc8447bc
nilesh1114 <nileshkr1114@gmail.com>
2024-12-23 12:23:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "powertool1107"
-    key            = "lab4/terraform.tfstate"
+    key            = "ansible/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "powertool"
